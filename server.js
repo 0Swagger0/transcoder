d4147a825a41fe8ef0a00e15a0abb0cd4cffdb06
Arjun Meena <meenaarjun229@gamil.com>
2024-07-10 21:19:15 +0530

    update deploy

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
 const express = require("express");
-const ytdl = require("ytdl-core");
+const ytdl = require("@distube/ytdl-core");
 const cors = require("cors");
-
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-app.get("/stream", (req, res) => {
+app.get("/stream", async (req, res) => {
   const videoId = req.query.id;
 
   if (!videoId) {
     return res.status(400).send("Missing YouTube video ID");
   }
 
-  const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
-
-  if (!ytdl.validateURL(videoUrl)) {
-    return res.status(400).send("Invalid YouTube video ID");
-  }
-
   try {
-    const audioStream = ytdl(videoUrl, { quality: "highestaudio" });
+    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    const stream = ytdl(videoUrl, { quality: "highestaudio" });
 
     res.setHeader("Content-Type", "audio/mpeg");
-    audioStream.pipe(res);
+    stream.pipe(res);
   } catch (err) {
     console.error("Stream error:", err);
     res.status(500).send("Failed to stream audio");
