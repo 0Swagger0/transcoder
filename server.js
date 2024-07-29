114e41c480edbe8ddf529c860406e3381f9c6feb
Arjun Meena <meenaarjun229@gamil.com>
2024-07-29 23:25:21 +0530

    -

--- server.js
+++ server.js
@@ -15,27 +15,19 @@ app.get("/stream", async (req, res) => {
     return res.status(400).send("Missing YouTube video ID");
   }
 
-  ytstream.setGlobalHeaders({
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    Referer: "https://www.youtube.com/",
-    Origin: "https://www.youtube.com",
-    Connection: "keep-alive",
-    "Cache-Control": "max-age=0",
-    "Upgrade-Insecure-Requests": "1",
+  // ytstream.setGlobalHeaders({
+  //   "User-Agent":
+  //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+  //   "Accept-Language": "en-US,en;q=0.9",
+  //   Referer: "https://www.youtube.com/",
+  //   Origin: "https://www.youtube.com",
+  //   Connection: "keep-alive",
+  //   "Cache-Control": "max-age=0",
+  //   "Upgrade-Insecure-Requests": "1",
 
-    Accept:
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-  });
-
-  const agent = new ytstream.YTStreamAgent([], {
-    keepAlive: true,
-    keepAliveMsecs: 5e3,
-  });
-
-  ytstream.setGlobalAgent(agent);
-  agent.syncFile(`./cookies.json`);
+  //   Accept:
+  //     "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+  // });
 
   // Base user agent string with a placeholder for the Gecko date
   const baseUserAgent =
@@ -54,6 +46,7 @@ app.get("/stream", async (req, res) => {
 
   const geckoDate = getRandomGeckoDate();
   ytstream.userAgent = baseUserAgent.replace("[DATE]", geckoDate);
+
   try {
     const stream = await ytstream.stream(videoUrl, {
       download: true,
