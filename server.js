6670dd379ce9587e69f2ab2c9a21b989cac3c17b
Swagger <swagger@192.168.1.14>
2024-08-01 21:51:01 +0530

    -

--- server.js
+++ server.js
@@ -5,36 +5,15 @@ const puppeteer = require("puppeteer");
 const app = express();
 const port = 3000;
 
-const COOKIE_PATH = "cookies.json";
-
-// Function to generate cookies using Puppeteer
-async function refreshCookies() {
-  // Launch Puppeteer with specified Chrome path or default
-  const browser = await puppeteer.launch({
-    headless: true, // Run in headless mode
-  });
-  const page = await browser.newPage();
-  await page.goto("https://www.youtube.com"); // Visit YouTube to get cookies
-  const cookies = await page.cookies();
-  fs.writeFileSync(COOKIE_PATH, JSON.stringify(cookies));
-
-  await browser.close();
-}
-
 app.get("/stream", async (req, res) => {
   const videoId = req.query.id;
 
   if (!videoId) {
     return res.status(400).send("Missing YouTube video ID");
   }
-  // const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json")));
-  // Refresh cookies if they don't exist or are outdated
-  if (!fs.existsSync(COOKIE_PATH)) {
-    await refreshCookies();
-  }
 
-  const cookies = JSON.parse(fs.readFileSync(COOKIE_PATH, "utf8"));
-  const agent = ytdl.createAgent(cookies); // Create an agent with cookies
+  // agent should be created once if you don't want to change your cookie
+  const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json")));
 
   const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
 
