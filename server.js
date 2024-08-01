31389331ce5e2d80b3fd1a030bbcf0556d5d7db4
Swagger <swagger@192.168.1.14>
2024-08-01 17:32:01 +0530

    -

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require("express");
 const ytdl = require("@distube/ytdl-core");
 const fs = require("fs");
-const path = require("path");
 const app = express();
 const port = 3000;
-
-// Function to update cookie expiration dates
-const updateCookiesExpiration = (cookies, futureDate) => {
-  return cookies.map((cookie) => {
-    cookie.expirationDate = futureDate;
-    return cookie;
-  });
-};
 
 app.get("/stream", async (req, res) => {
   const videoId = req.query.id;
@@ -26,15 +17,9 @@ app.get("/stream", async (req, res) => {
 
   try {
     // Read cookies from file and update expiration date
-    const cookiesPath = path.join(__dirname, "cookies.json");
-    let cookies = JSON.parse(fs.readFileSync(cookiesPath, "utf8"));
-
-    // Set a far future date for expiration, e.g., 10 years from now
-    const futureDate = Math.floor(Date.now() / 1000) + 10 * 365 * 24 * 60 * 60;
-    cookies = updateCookiesExpiration(cookies, futureDate);
 
     // Create an agent with updated cookies
-    const agent = ytdl.createAgent(cookies);
+    const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json")));
 
     ytdl(videoUrl, {
       agent,
