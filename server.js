f546fba82bfa58c84b6328205af1f379923a4f81
Swagger <swagger@192.168.1.14>
2024-08-01 14:39:06 +0530

    -

--- server.js
+++ server.js
@@ -14,7 +14,20 @@ app.get("/stream", async (req, res) => {
 
   const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
   // Base user agent string with a placeholder for the Gecko date
-  const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json")));
+  const agent = ytdl.createAgent([
+    {
+      domain: ".youtube.com",
+      expirationDate: 1234567890,
+      hostOnly: false,
+      httpOnly: true,
+      name: "AFmmF2swRAIgekjc3IZ5exa1gea6Uf5SXIpqwVssJpDYsv72Vzvpm_kCIEMSzuNSHSG0PPxlTsRnuwAgOKhy_On-GhXIGjD2lXQH:QUQ3MjNmekdoNGlmSXhhQ3R2WmxwaW0yLUMxcFo2Yl8xUGlqc2t6OS01SExVQTZxdXJGVklOZjdsNDYtNXl1VEV5MlRNRjZpZnlYTXJ6X0dyeXNVTDhibWdRRmRYWm9CWjhZdk5aV2lrTlZjZ0xFOHhwU2pPY041QlUzTzdIdlZ0VGFWSDBtSVlFdmhmendJVExkWnBxQTZpdW9ZcnNqWUFB",
+      path: "/",
+      sameSite: "no_restriction",
+      secure: true,
+      session: false,
+      value: "L9iA9XDqN65MaLtO/A6W9-HS5k3fy1gNoT",
+    },
+  ]);
 
   try {
     ytdl(videoUrl, {
