070afc3112f6adc9df7ba6464041a035b099a0fa
Swagger <swagger@192.168.1.14>
2024-08-01 16:56:07 +0530

    -

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const ytdl = require("@distube/ytdl-core");
 const fs = require("fs");
-require("dotenv").config();
+const path = require("path");
 const app = express();
 const port = 3000;
+
+// Function to update cookie expiration dates
+const updateCookiesExpiration = (cookies, futureDate) => {
+  return cookies.map((cookie) => {
+    cookie.expirationDate = futureDate;
+    return cookie;
+  });
+};
 
 app.get("/stream", async (req, res) => {
   const videoId = req.query.id;
@@ -12,11 +20,22 @@ app.get("/stream", async (req, res) => {
     return res.status(400).send("Missing YouTube video ID");
   }
 
-  const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json")));
+  // const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json")));
 
   const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
 
   try {
+    // Read cookies from file and update expiration date
+    const cookiesPath = path.join(__dirname, "cookies.json");
+    let cookies = JSON.parse(fs.readFileSync(cookiesPath, "utf8"));
+
+    // Set a far future date for expiration, e.g., 10 years from now
+    const futureDate = Math.floor(Date.now() / 1000) + 10 * 365 * 24 * 60 * 60;
+    cookies = updateCookiesExpiration(cookies, futureDate);
+
+    // Create an agent with updated cookies
+    const agent = ytdl.createAgent(cookies);
+
     ytdl(videoUrl, {
       agent,
       filter: "audioonly",
