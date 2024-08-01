f99ae67a54612a840169531f854b971905a69dd3
Swagger <swagger@192.168.1.14>
2024-08-01 23:53:47 +0530

    -

--- server.js
+++ server.js
@@ -2,9 +2,46 @@ const express = require("express");
 const ytdl = require("@distube/ytdl-core");
 const fs = require("fs");
 const puppeteer = require("puppeteer");
-const { getRandomIPv6 } = require("@distube/ytdl-core/lib/utils");
 const app = express();
+const path = require("path");
 const port = 3000;
+
+const COOKIES_PATH = path.resolve(__dirname, "cookies.json");
+
+// Function to update cookies using Puppeteer
+async function updateCookies() {
+  let existingCookies = [];
+
+  if (fs.existsSync(COOKIES_PATH)) {
+    const cookiesData = fs.readFileSync(COOKIES_PATH);
+    existingCookies = JSON.parse(cookiesData);
+  }
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.youtube.com");
+
+  const newCookies = await page.cookies();
+
+  const updatedCookies = existingCookies.map((cookie, index) => {
+    const newCookie = newCookies.find(
+      (c) => c.name === cookie.name && c.domain === cookie.domain
+    );
+    return {
+      ...cookie,
+      expirationDate: newCookie
+        ? newCookie.expires
+        : Date.now() / 1000 + 30 * 24 * 60 * 60,
+      id: index + 1,
+    };
+  });
+
+  fs.writeFileSync(COOKIES_PATH, JSON.stringify(updatedCookies, null, 2));
+
+  await browser.close();
+
+  return updatedCookies;
+}
 
 app.get("/stream", async (req, res) => {
   const videoId = req.query.id;
@@ -12,15 +49,14 @@ app.get("/stream", async (req, res) => {
   if (!videoId) {
     return res.status(400).send("Missing YouTube video ID");
   }
+  await updateCookies();
 
-  // agent should be created once if you don't want to change your cookie
-  const agent = ytdl.createAgent(undefined, {
-    localAddress: getRandomIPv6("2001:2::/48"),
-  });
-
-  const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+  const cookies = JSON.parse(fs.readFileSync(COOKIES_PATH));
+  const agent = ytdl.createAgent(cookies);
 
   try {
+    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+
     ytdl(videoUrl, {
       agent,
       filter: "audioonly",
