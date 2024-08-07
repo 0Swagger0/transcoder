0a0af43097b1bd049e68827eeb8cf78c35cacbc6
Swagger <swagger@192.168.1.12>
2024-08-07 13:39:58 +0530

    -

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const ytstream = require("yt-stream");
-const path = require("path");
 const port = 3000;
-
-ytstream.setApiKey(`AIzaSyAGA9Qf7bwq96eFE5GhAEAgQGmDryMlFNA`);
-ytstream.setPreference("api", "ANDROID");
-ytstream.setPreference("scrape");
 
 app.get("/stream", async (req, res) => {
   const videoId = req.query.id;
@@ -18,25 +13,19 @@ app.get("/stream", async (req, res) => {
 
   const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
 
-  const agent = new ytstream.YTStreamAgent([], {
-    localAddress: "10.244.53.138",
-    keepAlive: true,
-    keepAliveMsecs: 5e3,
-  });
-
-  agent.syncFile(path.join(__dirname, `./cookies.json`));
-
   try {
-    const stream = await ytstream.stream(videoUrl, {
-      download: true,
-      quality: "high",
-      highWaterMark: 1048576 * 32,
-      type: "audio",
-    });
-    stream.stream.pipe(res);
-
-    console.error("Error fetching audio stream:", error);
-    res.status(500).send("Error fetching audio stream");
+    try {
+      const stream = await ytstream.stream(videoUrl, {
+        download: true,
+        quality: "high",
+        highWaterMark: 1048576 * 32,
+        type: "audio",
+      });
+      stream.stream.pipe(res);
+    } catch (error) {
+      console.error("Error fetching audio stream:", error);
+      res.status(500).send("Error fetching audio stream");
+    }
   } catch (error) {
     console.error("Error fetching audio stream:", error);
     res.status(500).send("Error fetching audio stream");
