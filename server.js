d1afa268fe2d94465529dc491c13199655b1eb5b
Swagger <swagger@192.168.1.12>
2024-08-07 14:34:40 +0530

    -

--- server.js
+++ server.js
@@ -10,21 +10,15 @@ app.get("/stream", async (req, res) => {
   if (!videoId) {
     return res.status(400).send("Missing YouTube video ID");
   }
-  ytstream.setApiKey("AIzaSyAGA9Qf7bwq96eFE5GhAEAgQGmDryMlFNA");
-  ytstream.setPreference("api", "ANDROID");
-  ytstream.setGlobalHeaders({
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    Referer: "https://www.youtube.com/",
-    DNT: "1", // Do Not Track
-    Connection: "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
-    TE: "Trailers",
-  });
 
   const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+
+  ytstream.setApiKey("AIzaSyAGA9Qf7bwq96eFE5GhAEAgQGmDryMlFNA");
+  ytstream.setPreference("api", "ANDROID");
+  ytstream.setPreference("scrape");
+
+  ytstream.setGlobalAgent(JSON.parse(fs.readFileSync("cookies.json")));
+
   try {
     const stream = await ytstream.stream(videoUrl, {
       download: true,
