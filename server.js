7d565c56a29f52fded34a8420589ba36252a16a0
Swagger <swagger@192.168.1.12>
2024-08-08 07:33:03 +0530

    -

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const ytdl = require("@distube/ytdl-core");
+const ytstream = require("yt-stream");
 const path = require("path");
 const port = 3000;
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
-
-const agentOptions = {
-  pipelining: 5,
-  maxRedirections: 0,
-};
-
-const agent = ytdl.createAgent(
-  JSON.parse(fs.readFileSync("cookies.json")),
-  agentOptions
-);
 
 app.get("/stream", async (req, res) => {
   const videoId = req.query.id;
@@ -27,13 +17,24 @@ app.get("/stream", async (req, res) => {
 
   const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
 
+  ytstream.setApiKey("AIzaSyAGA9Qf7bwq96eFE5GhAEAgQGmDryMlFNA");
+  ytstream.setPreference("api", "ANDROID");
+  ytstream.setPreference("scrape");
+
+  const agent = new ytstream.YTStreamAgent(
+    JSON.parse(fs.readFileSync("cookies.json"))
+  );
+  ytstream.setGlobalAgent(agent);
+
   try {
-    ytdl(videoUrl, {
-      agent,
-      filter: "audioonly",
-    }).pipe(res);
+    const stream = await ytstream.stream(videoUrl, {
+      download: true,
+      quality: "high",
+      type: "audio",
+    });
 
     res.setHeader("Content-Type", "audio/mpeg");
+    stream.stream.pipe(res);
   } catch (error) {
     console.error("Error fetching audio stream:", error);
     res.status(500).send("Error fetching audio stream");
