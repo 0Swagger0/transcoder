d4dc52c2b1f01abb0bc523ace5c4208f41caa1a0
Swagger <swagger@192.168.1.9>
2024-09-24 15:39:46 +0530

    -

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const path = require("path");
 const port = 3000;
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const { default: axios } = require("axios");
+const https = require("https");
 puppeteer.use(StealthPlugin());
+
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 const agentOptions = {
   pipelining: 5,
@@ -25,15 +29,13 @@ app.get("/stream", async (req, res) => {
     return res.status(400).send("Missing YouTube video ID");
   }
 
-  const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
-
   try {
     ytdl(videoUrl, {
       agent,
       requestOptions: {
         headers: {
           "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         },
       },
       filter: "audioonly",
@@ -43,6 +45,27 @@ app.get("/stream", async (req, res) => {
   } catch (error) {
     console.error("Error fetching audio stream:", error);
     res.status(500).send("Error fetching audio stream");
+  }
+});
+
+// get lyrics
+app.get("/lyrics", async (req, res) => {
+  const title = req.query.title;
+
+  if (!title) {
+    return res.status(400).send("Missing artist or title");
+  }
+
+  try {
+    const response = await axios.get(
+      `https://api.textyl.co/api/lyrics?q=${title}`,
+      {
+        httpsAgent,
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching lyrics:", error);
   }
 });
 
